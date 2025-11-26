c525638e6f601ebd22248d037c7b0f5813b11014
Anand Sagar <anandsagar0006@gmail.com>
2025-11-26 17:17:19 +0530

    correcting application.properties

--- src/main/java/com/rideer/backend/config/S3Config.java
+++ src/main/java/com/rideer/backend/config/S3Config.java
@@ -12,36 +12,35 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 @Configuration
 public class S3Config {
 
-    @Value("${aws.access-key}")
+    @Value("${aws.s3.bucket}")
+    private String bucketName;
+
+    @Value("${aws.s3.region}")
+    private String region;
+
+    @Value("${aws.s3.access-key}")
     private String accessKey;
 
-    @Value("${aws.secret-key}")
+    @Value("${aws.s3.secret-key}")
     private String secretKey;
-
-    @Value("${aws.region}")
-    private String region;
 
     @Bean
     public S3Client s3Client() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(accessKey, secretKey)
-                        )
-                )
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
     }
 
     @Bean
     public S3Presigner s3Presigner() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+
         return S3Presigner.builder()
                 .region(Region.of(region))
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(accessKey, secretKey)
-                        )
-                )
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
     }
 }
